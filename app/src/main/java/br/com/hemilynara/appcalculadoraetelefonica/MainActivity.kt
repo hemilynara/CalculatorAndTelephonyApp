ce2cd626093a8e630391128586f4034bbe7ff847
hemilynara <hemilynara@gmail.com>
2024-04-10 22:53:32 -0300

    commit inicial

---
+++ app/src/main/java/br/com/hemilynara/appcalculadoraetelefonica/MainActivity.kt
@@ -0,0 +1,53 @@
+package br.com.hemilynara.appcalculadoraetelefonica
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Capturando os elementos por ID
+        val buttonCalc = findViewById<Button>(R.id.buttonCalc)
+        val buttonPhone = findViewById<Button>(R.id.buttonPhone)
+        val buttonStudent = findViewById<Button>(R.id.buttonStudent)
+
+        // Definir ações ao tap do botão -> Ir para a tela da calculadora
+        buttonCalc.setOnClickListener {
+            val intent = Intent(this, CalculatorActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Definir ações ao tap do botão -> Ir para a tela da conta telefônica
+        buttonPhone.setOnClickListener {
+            val intent = Intent(this, PhoneActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Chamada da função de exibição de assinatura
+        buttonStudent.setOnClickListener {
+            showSignature()
+        }
+    }
+
+    // Função para exibir a assinatura do aplicativo
+    private fun showSignature() {
+        val mySignature = """
+            Hemily Nara da Silva (RM550889)
+        """.trimIndent()
+
+        AlertDialog.Builder(this)
+            .setTitle("Desenvolvido Por")
+            .setMessage(mySignature)
+            .setPositiveButton("Ok") { dialog, which ->
+
+            }
+            .show()
+    }
+
+}
---
+++ app/src/main/java/br/com/hemilynara/appcalculadoraetelefonica/PhoneActivity.kt
@@ -0,0 +1,63 @@
+package br.com.hemilynara.appcalculadoraetelefonica
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class PhoneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone)
+
+        // Capturando os elementos por ID
+        val valorFixo = findViewById<EditText>(R.id.textoFixo)
+        val valorEdit1 = findViewById<EditText>(R.id.textoValor1)
+        val valorEdit2 = findViewById<EditText>(R.id.textoValor2)
+        val buttonCalc = findViewById<Button>(R.id.button)
+
+        // Colocando evento no botão para fazer o cálculo e já exibir na tela seguinte
+        buttonCalc.setOnClickListener {
+
+            // Validação de tipo de dado no campo (para que não seja string, nem nulo)
+            val taxa = valorFixo.text.toString().toDoubleOrNull()
+            val valor1 = valorEdit1.text.toString().toDoubleOrNull()
+            val valor2 = valorEdit2.text.toString().toDoubleOrNull()
+
+            // Tratativa de erro -> Adiciona um Toast para caso "" seja digitado nos campos de valor
+            if (valor1 == null || valor2 == null || taxa == null) {
+                Toast.makeText(this, "Os campos de valor são obrigatórios.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            // Matemática dos valores de acordo com projeto
+            val resultado1 = valor1 * 0.04
+            val resultado2 = valor2 * 0.20
+
+            // Variável que salva os valores para exibição na tela seguinte
+            val intent = Intent(this, ResultActivity::class.java).apply {
+                putExtra("taxa", taxa)
+                putExtra("resultado1", resultado1)
+                putExtra("resultado2", resultado2)
+            }
+            startActivity(intent)
+        }
+
+        // Habilita o botão de voltar na barra de ação
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    // Tratar o evento de clique no botão de voltar da barra de ação
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed() // Simular o comportamento do botão de voltar físico
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
